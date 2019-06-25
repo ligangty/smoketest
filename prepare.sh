00b316b09c999902ba70beb7a30421caf6f8d758
Gang Li <gli@redhat.com>
2019-06-25 17:06:42 +0800

    Update: add prepare script for env initialization, and some conf files

---
+++ prepare.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+THIS=$(cd ${0%/*} && echo $PWD/${0##*/})
+BASEDIR=`dirname ${THIS}`
+GITDIR=$BASEDIR/git
+INDYDIR=$BASEDIR/indy
+AQDIR=$BASEDIR/auditquery
+
+rm -rf $INDYDIR/pkg && mkdir -p $INDYDIR/pkg
+rm -rf $AQDIR/pkg && mkdir -p $AQDIR/pkg
+mkdir -p $GITDIR
+
+prepare() 
+{
+    repo=$1
+    src_tar=$2
+    dest_tar=$3
+    if [ ! -f $GITDIR/$repo/pom.xml ];
+    then
+        git clone git@github.com:ligangty/$repo.git $GITDIR/$repo
+    fi
+    cd $GITDIR/$repo
+    git fetch origin 
+    git checkout changelog
+    git reset --hard origin/changelog
+    mvn clean install -DskipTests
+    cp $src_tar $dest_tar    
+}
+
+prepare indy $GITDIR/indy/deployments/launcher/target/indy-launcher-*-complete.tar.gz $INDYDIR/pkg/indy.tar.gz
+cd $INDYDIR
+docker build -t commonjava/smoke-indy:latest .
+
+prepare auditquery $GITDIR/auditquery/deployments/standalone-rest/target/auditquery-standalone-rest-*.tar.gz $AQDIR/pkg/auditquery.tar.gz
+cd $AQDIR
+docker build -t commonjava/smoke-aq:latest .
